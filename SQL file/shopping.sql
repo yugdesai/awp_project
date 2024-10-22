0227c3d6eccb8b7e3986b94839dd5d89543d30a0
Yug Desai <97080880+yugdesai@users.noreply.github.com>
2024-10-22 14:45:51 +0530

    Add files via upload

---
+++ SQL file/shopping.sql
@@ -0,0 +1,5 @@
+<form ng-submit="loginUser()">
+    <input type="text" ng-model="username" placeholder="Username" required />
+    <input type="password" ng-model="password" placeholder="Password" required />
+    <button type="submit">Login</button>
+</form>
